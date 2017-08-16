66e417fa640c8b3d185d8fa894ab778cbeefa841
briankeane <lonesomewhistle@gmail.com>
2017-08-16 06:42:46 -0500

    basic setup

---
+++ PlayolaCore/PlayolaCoreIOS/PlayolaCoreIOS.h
@@ -0,0 +1,19 @@
+//
+//  PlayolaCoreIOS.h
+//  PlayolaCoreIOS
+//
+//  Created by Brian D Keane on 8/16/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayolaCoreIOS.
+FOUNDATION_EXPORT double PlayolaCoreIOSVersionNumber;
+
+//! Project version string for PlayolaCoreIOS.
+FOUNDATION_EXPORT const unsigned char PlayolaCoreIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayolaCoreIOS/PublicHeader.h>
+
+
---
+++ PlayolaCore/PlayolaCoreOSX/PlayolaCoreOSX.h
@@ -0,0 +1,19 @@
+//
+//  PlayolaCoreOSX.h
+//  PlayolaCoreOSX
+//
+//  Created by Brian D Keane on 8/16/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PlayolaCoreOSX.
+FOUNDATION_EXPORT double PlayolaCoreOSXVersionNumber;
+
+//! Project version string for PlayolaCoreOSX.
+FOUNDATION_EXPORT const unsigned char PlayolaCoreOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayolaCoreOSX/PublicHeader.h>
+
+
